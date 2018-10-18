fa2150c5a2f9e9a31e24386545d2f87cbbe85a22
Jack Lu <jlu9@stuy.edu>
2018-10-18 13:55:34 -0400

    working login and welcome

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def root():
 def landing():
 	return render_template("splash.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def redir():
     return redirect(url_for("welcome"))
 
 @app.route("/welcome")
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html", name='bni')
 
+app.debug = True
 app.run()
